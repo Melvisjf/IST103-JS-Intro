f64c8fa58fd362465532028f580b195a30cb2843
JU4N <juanjoflorez_@hotmail.com>
2024-04-11 14:34:33 -0700

    console_log

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Hello world")
+
+//Activity 2
+let Name = "Juan";
+let age = "24";
+let height = "1.76";
+
+console.log("My names is " + Name);
+console.log("my age is " + age);
+console.log("my height is " + height);
+
+

4e0ee827f9c52b83b28772f621b7a5b6dabe3191
JU4N <juanjoflorez_@hotmail.com>
2024-04-11 15:18:03 -0700

    activity

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 
 //Activity 2
-let Name = "Juan";
-let age = "24";
-let height = "1.76";
+let Name = "Juan"
+let age = "24"
+let height = "1.76"
 
-console.log("My names is " + Name);
-console.log("my age is " + age);
-console.log("my height is " + height);
+console.log("My names is " + Name)
+console.log("my age is " + age)
+console.log("my height is " + height)
 
+//Activity 3
+ let whattodo =[
+    "I have to study",
+    "I have to checkout the garbage",
+    "I have to prepare food"
+ ]
 
+ let myPhone = {
+    screen: "6.1 inches",
+    camera: "12px",
+    ram: "4gb",
+    battery: "3110mAh"
+ }
+
+console.log("what i need to do"+ whattodo[0])
+console.log("My phone specifications"+ myPhone)
+
+ 
